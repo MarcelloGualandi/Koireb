0d60e1899e33a5a31d07f3570e069e944e5af6d8
MarcelloGualandi <marcellogualandi96@gmail.com>
2026-04-30 15:01:23 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,10 @@
+
+# if else
+numeri <- function(x){
+  if(x>0){
+    print("Questo numero è positivo, ciuco!")
+    }
+  else {
+    print("Questo numero è negativo, se non sai queste cose, torna a scuola")  
+    }
+  }
